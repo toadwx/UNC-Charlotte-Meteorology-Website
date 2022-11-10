ea27c26b0980079692ec4ae9d10e361a88c38fa0
Matthew Toadvine <toad1225@gmail.com>
2022-11-09 19:36:51 -0500

    Initial commit for faculty page
    Includes a source change for current conditions image

---
+++ faculty-content.php
@@ -0,0 +1,22 @@
+<div class="main faculty">
+
+    <center>
+    
+    <h3>Meteorology Faculty</h3>
+    
+    <div class="img-block">
+        <img src="images/faculty/cdavenport.jpg" />
+        <img src="images/faculty/meastin.jpg" />
+        <img src="images/faculty/bmagi.jpg" />
+    </div>
+    
+    <div class="img-block">
+        <img src="images/faculty/jscheff.jpg" />
+        <img src="images/faculty/tshirley.jpg" />
+    </div>
+    
+    </center>
+    
+    <br/>
+
+</div>
---
+++ faculty.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <?php include("header.php");?>
+    </head>
+    <body>
+        <?php include("navbar.php");?>
+        
+        <?php include("faculty-content.php");?>
+
+        <?php include("footer.php");?>
+    </body>
+</html>
