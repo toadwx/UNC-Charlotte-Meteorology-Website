767069c7351d9494eed9d1ea352db3066b772234
Matthew Toadvine <toad1225@gmail.com>
2022-12-06 22:25:18 -0500

    Faculty Page Update
    Nearly complete (barring small changes). Added overlays on mouse hover to display name and titles with links to their personal university webpages

--- faculty-content.php
+++ faculty-content.php
@@ -5,14 +5,59 @@
     <h3>Meteorology Faculty</h3>
     
     <div class="img-block">
-        <img src="images/faculty/cdavenport.jpg" />
-        <img src="images/faculty/meastin.jpg" />
-        <img src="images/faculty/bmagi.jpg" />
+        <div class="container">
+            <img src="images/faculty/cdavenport.jpg" alt="Casey Davenport"/>
+            <div class="overlay">
+                <div class="name">Dr. Casey Davenport</div>
+                <div class="prof-title">Associate Professor</div>
+                <form method="POST" target="_blank" action="http://clas-pages.uncc.edu/casey-davenport/">
+                  <button class="info-btn" id="cdavenport" type="submit">More</button>
+                </form>
+          </div>
+        </div>
+        <div class="container">
+            <img src="images/faculty/meastin.jpg" alt="Matthew Eastin"/>
+            <div class="overlay">
+                <div class="name">Dr. Matthew Eastin</div>
+                <div class="prof-title">Associate Professor</div>
+                <form method="POST" target="_blank" action="http://clas-pages.uncc.edu/matt-eastin/">
+                  <button class="info-btn" id="meastin" type="submit">More</button>
+                </form>
+          </div>
+        </div>
+        <div class="container">
+            <img src="images/faculty/bmagi.jpg" alt="Brian Magi"/>
+            <div class="overlay">
+                <div class="name">Dr. Brian Magi</div>
+                <div class="prof-title">Associate Professor<br/>Earth Sciences Graduate Coordinator</div>
+                <form method="POST" target="_blank" action="http://brianmagi.uncc.edu/">
+                  <button class="info-btn" id="bmagi" type="submit">More</button>
+                </form>
+          </div>
+        </div>
     </div>
     
     <div class="img-block">
-        <img src="images/faculty/jscheff.jpg" />
-        <img src="images/faculty/tshirley.jpg" />
+        <div class="container">
+            <img src="images/faculty/jscheff.jpg" alt="Jack Scheff"/>
+            <div class="overlay">
+                <div class="name">Dr. Jack Scheff</div>
+                <div class="prof-title">Assistant Professor</div>
+                <form method="POST" target="_blank" action="https://clas-pages.uncc.edu/hcl/">
+                  <button class="info-btn" id="jscheff" type="submit">More</button>
+                </form>
+          </div>
+        </div>
+        <div class="container">
+            <img src="images/faculty/tshirley.jpg" alt="Terry Shirley"/>
+            <div class="overlay">
+                <div class="name">Terry Shirley</div>
+                <div class="prof-title">Senior Lecturer<br/>Department Associate Chair<br/>Meteorology Undergraduate Coordinator</div>
+                <form method="POST" target="_blank" action="https://geoearth.charlotte.edu/terry-shirley">
+                  <button class="info-btn" id="tshirley" type="submit">More</button>
+                </form>
+          </div>
+        </div>
     </div>
     
     </center>
