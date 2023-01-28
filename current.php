654c0cf98f8d7e32caf5e76d14b821f3991cb7db
WEP11 <wepettee@gmail.com>
2023-01-28 10:46:08 -0600

    Adds custom date field

--- current.php
+++ current.php
@@ -9,27 +9,30 @@
         <h3>Current Conditions</h3>
         <div>
             <label class="radio-container">24 hours
-                <input type="radio" checked="checked" name="timescaleRadio">
+                <input type="radio" name="timescaleRadio" value="24-hr">
                 <span class="radio-checkmark"></span>
             </label>
             <label class="radio-container">48 hours
-                <input type="radio" name="timescaleRadio">
+                <input type="radio" checked="checked" name="timescaleRadio" value="48-hr">
                 <span class="radio-checkmark"></span>
             </label>
             <label class="radio-container">72 hours
-                <input type="radio" name="timescaleRadio">
+                <input type="radio" name="timescaleRadio" value="3-day">
                 <span class="radio-checkmark"></span>
             </label>
+            <label class="radio-container">Custom 24 hour period ending:
+                <input type="radio" name="timescaleRadio" value="custom">
+                <span class="radio-checkmark"></span>
+            </label>
+            <input id="customDatePick" type="date" name="customDatePick">
         </div>
         <div class="tab">
             <button id="defaultTabButton" class="tablinks active" onclick="openTab(event, 'tempChartCont')">Temperatures</button>
             <button class="tablinks" onclick="openTab(event, 'prcpChartCont')">Precipitation</button>
-            <button class="tablinks" onclick="openTab(event, 'presChartCont')">Pressure</button>
             <button class="tablinks" onclick="openTab(event, 'windChartCont')">Wind</button>
         </div>
         <div id="tempChartCont" class="tabcontent" style="display:block"><div id="tempChart"></div></div>
         <div id="prcpChartCont" class="tabcontent"><div id="prcpChart"></div></div>
-        <div id="presChartCont" class="tabcontent"><div id="presChart"></div></div>
         <div id="windChartCont" class="tabcontent"><div id="windChart"></div></div>
 
         </div>
