5dd02c5911f3f619bddbab128a28be21b8c9afe3
WEP11 <wepettee@gmail.com>
2023-03-18 08:33:59 -0500

    Adds HighCharts Export module

--- header.php
+++ header.php
@@ -5,3 +5,4 @@
 <link rel="stylesheet" href="css/footer.css">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
